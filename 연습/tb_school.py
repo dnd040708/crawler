8d502d2dd9cb6f900d028c65cb2413a6ad0cb520
dnd040708 <43291350+dnd040708@users.noreply.github.com>
2023-11-22 12:53:49 +0900

    Update tb_school.py

--- 연습/tb_school.py
+++ 연습/tb_school.py
@@ -1,22 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
 import requests
-import api_key 
+from common.api_key import *
+import pandas as pd
 
 seed_url = 'http://api.data.go.kr/openapi/tn_pubr_public_elesch_mskul_lc_api'
 params = {
-    'serviceKey' : api_key.api_key,
-    'pageNo' : '2000',
+    'serviceKey' : api_key['school'],
+    'pageNo' : '20000',
     'numOfRows' : '100',
     'type' : 'json',
+    'cddcCode' : '7010000' # 서울특별시교육청 코드
 }
 
-page = 1
-with requests.Session() as session:
-    while True:
-        params['pageNo'] = str(page)
-        resp = session.get(url = seed_url, params=params)
-        data = resp.json()
-        if data['response']['header']['resultMsg'] == 'NODATA_ERROR':
-            break
-        for item in data['response']['body']['items']:
-            print(item)
-        page += 1
+def append_dict(count, target_pandas, input_dict):
+    if count == 1:
+        target_pandas = pd.DataFrame([input_dict])
+    else:
+        target_pandas = pd.concat([target_pandas, pd.DataFrame([input_dict])])
+    return target_pandas
+
+def crawl():
+    page = 1
+    count = 1
+    result = ''
+    with requests.Session() as session:
+        while True:
+            params['pageNo'] = str(page)
+            resp = session.get(url = seed_url, params=params)
+            data = resp.json()
+            if data['response']['header']['resultMsg'] == 'NODATA_ERROR':
+                result.to_csv('./school.csv', encoding= 'utf-8-sig', index = False)
+                break
+            for item in data['response']['body']['items']:
+                model_kwargs = {
+                    'schoolid': item['schoolId']
+                    ,'schoolnm': item['schoolNm']
+                    ,'schoolse': item['schoolSe']
+                    ,'fonddate': item['fondDate']
+                    ,'fondtype': item['fondType']
+                    ,'bnhhse': item['bnhhSe']
+                    ,'opersttus': item['operSttus']
+                    ,'lnmadr': item['lnmadr']
+                    ,'rdnmadr': item['rdnmadr']
+                    ,'cddccode': item['cddcCode']
+                    ,'cddcnm': item['cddcNm']
+                    ,'edcsport': item['edcSport']
+                    ,'edcsportNm': item['edcSportNm']
+                    ,'creatdate': item['creatDate']
+                    ,'changedate': item['changeDate']
+                    ,'latitude': item['latitude']
+                    ,'longitude': item['longitude']
+                    ,'referencedate': item['referenceDate']
+                    ,'insttcode': item['insttCode']
+                }
+                result = append_dict(count, result, model_kwargs)
+                count += 1
+            page += 1
+
+crawl()
