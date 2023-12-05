cc1af4d18a0b522b87dc72d04d1d5cbaf8c04f87
dnd040708 <43291350+dnd040708@users.noreply.github.com>
2023-12-05 22:25:28 +0900

    Update 좌표변환.py

--- 연습/좌표변환.py
+++ 연습/좌표변환.py
@@ -1,15 +1,23 @@
 import pandas as pd
 
+def KATEC_to_wgs84(x, y):
+    WGS84 = "+proj=latlong +datum=WGS84 +ellps=WGS84"
+    KATEC = "+proj=tmerc +lat_0=38.0 +lon_0=128.0 +x_0=400000.0 +y_0=600000.0 +k=0.9999 +ellps=bessel +a=6377397.155 +b=6356078.9628181886 +units=m +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43"
 
-#open_csv = pd.read_csv("./aptlist.csv", encoding= 'utf-8-sig')
-#open_csv['T_X'] = To_x(KATEC_to_wgs84(open_csv['latitude'], open_csv['longitude']))
-#open_csv['T_Y'] = To_y(KATEC_to_wgs84(open_csv['latitude'], open_csv['longitude']))
-#open_csv['T_X'] = open_csv['T_X'].apply(lambda x : round(x))
-#open_csv['T_Y'] = open_csv['T_Y'].apply(lambda x : round(x))
-#open_csv.to_csv('./aptlist_table.csv', encoding= 'utf-8-sig', index= False)
+    KATEC_proj = pyproj.CRS(KATEC)
+    WGS84_proj = pyproj.CRS(WGS84)
+    trans_func = pyproj.Transformer.from_crs( WGS84_proj, KATEC_proj, always_xy=True)
+    return trans_func.transform(y, x)
 
-open_csv1 = pd.read_csv("./school_table.csv", encoding= 'utf-8-sig', usecols=[0, 19, 20])
-open_csv2 = pd.read_csv("./aptlist_table.csv", encoding= 'utf-8-sig', usecols=[0, 18, 19])
+def To_x(input_point):
+    return input_point[0]
 
-result = open_csv1['T_X'] -  open_csv2['T_X']
-print(result)
+def To_y(input_point):
+    return input_point[1]
+
+open_csv = pd.read_csv("./school.csv", encoding= 'utf-8-sig')
+open_csv['T_X'] = To_x(KATEC_to_wgs84(open_csv['latitude'], open_csv['longitude']))
+open_csv['T_Y'] = To_y(KATEC_to_wgs84(open_csv['latitude'], open_csv['longitude']))
+open_csv['T_X'] = open_csv['T_X'].apply(lambda x : round(x))
+open_csv['T_Y'] = open_csv['T_Y'].apply(lambda x : round(x))
+open_csv.to_csv('./school_job.csv', encoding= 'utf-8-sig', index= False)
